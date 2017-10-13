892d7843013bfda84cdaf2c94ccd83a97c68bd26
Skaral <none>
2017-10-13 18:35:21 +0200

    Vytvoři jsem zvášť složky.

---
+++ spaceships/GameObjects/GameObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace spaceships.GameObjects
+{
+    class GameObject                           // Objekt herního světa
+    {
+        public Point Pos { get; set; }         // Pozice objektu
+        public Vector Vec { get; set; }        // Směrový vektor
+    }
+}
--- spaceships/GameObjects/SpaceShip/SpaceShip.cs
+++ spaceships/GameObjects/SpaceShip/SpaceShip.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using spaceships.GameObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
-namespace spaceships
+namespace spaceships.GameObjects.SpaceShip
 {
     class SpaceShip : GameObject
     {
